00399d87f6b8ffee55059544d8caed253c69797d
kirill.shmakov <kirill.shmakov@jetbrains.com>
2019-02-08 13:08:27 +0300

    Ktor server introduced according to guide.

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "Thermo"
+version = "1.0-SNAPSHOT"
+
+val ktor_version = "1.1.2"
+
+plugins {
+    application
+    kotlin("jvm") version "1.3.21"
+}
+
+repositories {
+    jcenter()
+    maven("http://kotlin.bintray.com/ktor")
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<KotlinCompile>().all {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClassName = "MainKt"
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile("io.ktor:ktor-server-netty:$ktor_version")
+    compile("ch.qos.logback:logback-classic:1.2.3")
+    testCompile(group = "junit", name = "junit", version = "4.12")
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+package thermo
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hi from Thermo", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
